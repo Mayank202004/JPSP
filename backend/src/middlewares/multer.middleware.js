39058079bd8eeaf1f8348d1d58d99c8d97d905ca
Mayank202004 <136550347+Mayank202004@users.noreply.github.com>
2025-01-19 19:23:23 +0530

    Multer setup complete
    Resolves #6
    Multer setup complete with cloudinary upload util. FIle can not be taken into temporary storage on server and then uploaded on cloudinary and is deleted later after uploading.

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import path from 'path';
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp');
+    },
+    filename: function (req, file, cb) {
+      // Generate a random unique file name with a secure hash
+      const uniqueSuffix = crypto.randomBytes(16).toString('hex');
+      const extension = path.extname(file.originalname); // Get original file extension
+      cb(null, `${uniqueSuffix}${extension}`);
+    },
+  });
+
+export const upload = multer({ 
+    storage, 
+})
