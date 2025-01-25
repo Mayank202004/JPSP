563cf4072f72be32285868debf5d42821dfe0a0a
Mayank202004 <136550347+Mayank202004@users.noreply.github.com>
2025-01-25 19:04:07 +0530

    Added login and register forms
    Work on #15
    Added login and register forms

--- App/lib/views/auth/register.dart
+++ App/lib/views/auth/register.dart
@@ -1,154 +1,91 @@
-import 'package:flutter/gestures.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:jpss/controllers/auth_controller.dart';
-import 'package:jpss/utils/helper.dart';
-import '../../routes/route_names.dart';
-import '../../widets/auth_input.dart';
 import 'package:form_validator/form_validator.dart';
+import '../../widets/auth_input.dart';
 
-class Register extends StatefulWidget {
-  const Register({super.key});
+class RegisterForm extends StatelessWidget {
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final TextEditingController cpasswordController;
 
-  @override
-  State<Register> createState() => _RegisterState();
-}
-
-class _RegisterState extends State<Register> {
-  final GlobalKey<FormState> _form = GlobalKey<FormState>();
-  final TextEditingController namecontroller = TextEditingController(text: "");
-  final TextEditingController emailcontroller = TextEditingController(text: "");
-  final TextEditingController passwordcontroller = TextEditingController(
-      text: "");
-  final TextEditingController cpasswordcontroller = TextEditingController(
-      text: "");
-  final AuthController controller = Get.put(AuthController());
-
-  void sign_up() {
-    if (_form.currentState!.validate()) {
-      controller.register(
-          namecontroller.text,
-          emailcontroller.text,
-          passwordcontroller.text
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    emailcontroller.dispose();
-    namecontroller.dispose();
-    passwordcontroller.dispose();
-    cpasswordcontroller.dispose();
-    super.dispose();
-  }
+  const RegisterForm({
+    super.key,
+    required this.emailController,
+    required this.passwordController,
+    required this.nameController,
+    required this.cpasswordController,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(10.0),
-          child: Form(
-            key: _form,
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/images/JP.png",
-                  width: 100,
-                  height: 100,
-                ),
-                const Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                      Text(
-                          "Get started with JP's Scholarship Platform."
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                AuthInput(
-                  label: "Name",
-                  hintText: "Enter your name",
-                  controller: namecontroller,
-                  validatorCallback: ValidationBuilder().required()
-                      .minLength(3)
-                      .maxLength(50)
-                      .build(),
-                ),
-                const SizedBox(height: 20),
-                AuthInput(
-                  label: "Email",
-                  hintText: "Enter your email",
-                  controller: emailcontroller,
-                  validatorCallback: ValidationBuilder().required()
-                      .email()
-                      .build(),
-                ),
-                const SizedBox(height: 20),
-                AuthInput(
-                  label: "Password",
-                  hintText: "Enter your password",
-                  controller: passwordcontroller,
-                  isPasswordField: true,
-                  validatorCallback: ValidationBuilder().required()
-                      .minLength(8)
-                      .maxLength(50)
-                      .build(),
-                ),
-                const SizedBox(height: 20,),
-                AuthInput(
-                  label: "Confirm Password",
-                  hintText: "Confirm your password",
-                  controller: cpasswordcontroller,
-                  validatorCallback: (arg) {
-                    if (passwordcontroller.text != arg) {
-                      return "Confirm password doesnot match";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20,),
-                Obx(() {
-                  return ElevatedButton(
-                      onPressed: sign_up,
-                      style: ButtonStyle(
-                        minimumSize: WidgetStateProperty.all(
-                          const Size.fromHeight(40),
-                        ),
-                      ),
-                      child: Text(controller.registerLoading.value ? "Processing.." : "Sign up")
-                  );
-                }),
-                const SizedBox(height: 20,),
-                Text.rich(TextSpan(
-                    children: [
-                      TextSpan(text: "  Login",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () => Get.toNamed(RouteNames.login)
-                      )
-                    ],
-                    text: "Already have an account!"
-                ),
-                ),
-              ],
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Name"),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+            child: AuthInput(
+              controller: emailController,
+              validatorCallback: ValidationBuilder()
+                  .required()
+                  .build(),
             ),
           ),
-        ),
-      )
+          const Text("Email"),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+            child: AuthInput(
+              controller: emailController,
+              validatorCallback: ValidationBuilder()
+                  .required()
+                  .email()
+                  .build(),
+            ),
+          ),
+          const Text("Password"),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+            child: AuthInput(
+              isPasswordField: true,
+              controller: passwordController,
+              validatorCallback: ValidationBuilder()
+                  .required()
+                  .minLength(8)
+                  .maxLength(50)
+                  .build(),
+            ),
+          ),
+          const Text("Confirm Password"),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+            child: AuthInput(
+              isPasswordField: true,
+              controller: passwordController,
+              validatorCallback: ValidationBuilder()
+                  .required()
+                  .minLength(8)
+                  .maxLength(50)
+                  .build(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top :8.0,bottom: 24),
+            child: ElevatedButton.icon(
+                onPressed: (){},
+                style: ButtonStyle(
+                    shape: const WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20),))),
+                    backgroundColor: WidgetStatePropertyAll(Theme.of(context).primaryColor),
+                    foregroundColor: WidgetStatePropertyAll(Theme.of(context).indicatorColor),
+                    minimumSize: const WidgetStatePropertyAll(Size(double.infinity,56))
+                ),
+                icon: const Icon(CupertinoIcons.arrow_right),
+                label: const Text("Register")),
+          ),
+        ],
       ),
     );
+
   }
 }
