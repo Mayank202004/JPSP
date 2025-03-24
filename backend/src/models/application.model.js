ba943e0793c6209658d10eb79b53a7bd5612a507
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-03-24 23:34:31 +0530

    Create application.model.js
    Work on #35
    Added application schema

---
+++ backend/src/models/application.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const applicationSchema = new Schema(
+    {
+        profileId:{
+            type: Schema.Types.ObjectId,
+            ref: "Profile",
+        },
+        scholarshipId:{
+            type: Schema.Types.ObjectId,
+            ref: "Scholarship",
+        },
+        to: [{
+            authority: {
+              type: String,
+              required: true,
+            },
+            status: {
+              type: String,
+              enum: ["approved", "rejected", "pending","returned back to applicant"],
+              default: "pending",
+            }
+        }],
+        reviewComments: [{
+            type: String,
+            default: ""
+        }],
+        status: {
+            type: String,
+            enum: ["pending", "returned back to applicant", "approved", "rejected"],
+            default: "pending",
+        },
+        currentRecipient: {
+            type: String,
+            required: true
+        },  
+    },
+    {timestamps: true}
+);
+applicationSchema.index({ profileId: 1, scholarshipId: 1 });
+const Application = mongoose.model("Application", applicationSchema);
+export default Application;
