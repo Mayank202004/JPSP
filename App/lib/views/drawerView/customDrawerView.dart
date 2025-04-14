6969bbad1b5067c0d991f93eecfdd05bf579e627
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-04-14 19:08:30 +0530

    Work done on custom drawer view
    Work done on custom drawer view

--- App/lib/views/drawerView/customDrawerView.dart
+++ App/lib/views/drawerView/customDrawerView.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jpss/models/menuItem.dart';
+import 'package:rive/rive.dart';
 
 import '../../widets/drawerRow.dart';
 
@@ -12,12 +14,35 @@ class CustomDrawerView extends StatefulWidget {
 
 class _CustomDrawerViewState extends State<CustomDrawerView> {
   final List<MenuItem> _browseMenuIcons = MenuItem.menuItems;
+  final List<MenuItem> _historyMenuIcons = MenuItem.menuItems2;
+  final List<MenuItem> _themeMenuIcons = MenuItem.menuItems3;
+  String _selectedMenu = MenuItem.menuItems[0].title;
+  bool _isDarkMode = false;
+
+  void onMenuPress(MenuItem menu){
+    setState(() {
+      _selectedMenu = menu.title;
+    });
+  }
+
+  void onThemeToggle(value){
+    setState(() {
+      _isDarkMode = value;
+    });
+    _themeMenuIcons[0].riveIcon.status!.change(value);
+  }
+  
+  void onThemeRiveIconInit(Artboard artboard){
+    final controller = StateMachineController.fromArtboard(artboard, _themeMenuIcons[0].riveIcon.stateMachine);
+    artboard.addController(controller!);
+    _themeMenuIcons[0].riveIcon.status = controller.findInput<bool>("active") as SMIBool;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxWidth: 288),
-      decoration: BoxDecoration(
+      constraints: const BoxConstraints(maxWidth: 288),
+      decoration: const BoxDecoration(
         borderRadius: BorderRadius.only(bottomRight: Radius.circular(30),topRight: Radius.circular(30)),
         color: Color(0xFF22263E)
       ),
@@ -33,28 +58,94 @@ class _CustomDrawerViewState extends State<CustomDrawerView> {
                   CircleAvatar(
                     backgroundColor: Colors.white.withValues(alpha: 0.2),
                     foregroundColor: Colors.white,
-                    child: Icon(Icons.person_2_outlined),
+                    child: const Icon(Icons.person_2_outlined),
                   ),
-                  SizedBox(width: 8,),
+                  const SizedBox(width: 8,),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Text("Ramesh",style: TextStyle(color: Colors.white,fontSize: 17,fontFamily: "Inter"),),
+                      const Text("Ramesh",style: TextStyle(color: Colors.white,fontSize: 17,fontFamily: "Inter"),),
                       Text("X C",style: TextStyle(color: Colors.white.withValues(alpha: 0.7)),)
                     ],
                   )
                 ],
               ),
             ),
+            MenuButtonSection(title: "BROWSE", menuItems: _browseMenuIcons,selectedMenu: _selectedMenu,onMenuPress: onMenuPress,),
+            MenuButtonSection(title: "HISTORY", menuItems: _historyMenuIcons,selectedMenu: _selectedMenu,onMenuPress: onMenuPress),
+            const Spacer(),
             Padding(
-              padding: const EdgeInsets.only(left: 24,right: 24,top: 40,bottom: 8),
-              child: Text("Browse",style: TextStyle(color: Colors.white.withValues(alpha: 0.7),fontSize: 15,fontFamily: "Inter"),),
-            ),
-            DrawerRow(menu: _browseMenuIcons[0],)
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                      height: 32,
+                      width: 32,
+                      child: Opacity(
+                          opacity: 0.6,
+                          child: RiveAnimation.asset(
+                            'assets/rive/icons.riv',
+                            stateMachines: [_themeMenuIcons[0].riveIcon.stateMachine],
+                            artboard: _themeMenuIcons[0].riveIcon.artboard,
+                            onInit: onThemeRiveIconInit,
+                          )
+                      )
+                  ),
+                  Expanded(
+                    child: Text(
+                      _themeMenuIcons[0].title,
+                      style: const TextStyle(color: Colors.white,fontSize: 17,fontFamily: "Inter",fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  CupertinoSwitch(value: _isDarkMode, onChanged: onThemeToggle),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class MenuButtonSection extends StatelessWidget {
+  const MenuButtonSection({super.key, required this.title, required this.menuItems, this.selectedMenu = "Home", this.onMenuPress});
+
+  final String title;
+  final String selectedMenu;
+  final List<MenuItem> menuItems;
+  final Function(MenuItem menu)? onMenuPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24,right: 24,top: 40,bottom: 8),
+          child: Text(title,style: TextStyle(color: Colors.white.withValues(alpha: 0.7),fontSize: 15,fontFamily: "Inter"),),
+        ),
+        Container(
+          margin: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              for (var menu in menuItems) ...[
+                Divider(
+                  color: Colors.white.withValues(alpha: 0.1),
+                  thickness: 1,
+                  height: 1,
+                  indent: 16,
+                  endIndent: 16,
+                ),
+                DrawerRow(menu: menu,selectedMenu: selectedMenu,onMenuPress: () => onMenuPress!(menu),)
+              ]
+            ],
+          ),
+        )
+
+      ],
+    );
+  }
+}
+
--- App/lib/widets/drawerRow.dart
+++ App/lib/widets/drawerRow.dart
@@ -1,22 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jpss/models/menuItem.dart';
 import 'package:rive/rive.dart';
 
 class DrawerRow extends StatelessWidget {
-  const DrawerRow({super.key, required this.menu});
+  const DrawerRow({super.key, required this.menu, this.selectedMenu="Home", this.onMenuPress});
 
   final MenuItem menu;
+  final String selectedMenu;
+  final Function? onMenuPress;
+
+  void onMenuPressed(){
+    if(selectedMenu != menu.title){
+      onMenuPress!();
+      menu.riveIcon.status!.change(true);
+      Future.delayed(const Duration(seconds: 1),(){
+        menu.riveIcon.status!.change(false);
+      });
+    }
+  }
+
+  void _onMenuIconInit(Artboard artboard){
+    final controller = StateMachineController.fromArtboard(artboard, menu.riveIcon.stateMachine);
+    artboard.addController(controller!);
+    menu.riveIcon.status = controller.findInput<bool>("active") as SMIBool;
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
+      // This is the menu button background that animated as we click on it
       children: [
-        SizedBox(
-          width: 32,
-          height: 32,
-          //child: RiveAnimation.asset(),
-        )
-      ],
+        AnimatedContainer(
+          height: 56,
+          curve: const Cubic(0.2, 0.8, 0.2, 1),
+          width: selectedMenu == menu.title ? 288 -16 : 0,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10)
+          ), duration: const Duration(milliseconds: 300),
+        ),
+        CupertinoButton(
+          padding: const EdgeInsets.all(12),
+          onPressed: onMenuPressed,
+          pressedOpacity: 1,
+          child: Row(
+            children: [
+              SizedBox(
+                width: 32,
+                height: 32,
+                child: Opacity(
+                  opacity: 0.6,
+                  child: RiveAnimation.asset('assets/rive/icons.riv',
+                  stateMachines: [menu.riveIcon.stateMachine],
+                  artboard: menu.riveIcon.artboard,
+                  onInit: _onMenuIconInit,),
+                )
+              ),
+              const SizedBox(width: 14,),
+              Text(menu.title,style: const TextStyle(color: Colors.white,fontFamily: "Inter",fontWeight: FontWeight.w600),)
+            ],
+          ),
+        ),
+      ]
     );
   }
 }
