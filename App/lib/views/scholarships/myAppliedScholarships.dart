aa27db0da77bf24665db4f5438bf0ff68b209b28
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-04-20 22:32:38 +0530

    Create myAppliedScholarships.dart
    Work on #44

---
+++ App/lib/views/scholarships/myAppliedScholarships.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyAppliedSchemes extends StatelessWidget {
+  const MyAppliedSchemes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
