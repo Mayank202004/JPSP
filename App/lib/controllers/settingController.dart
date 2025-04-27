fc888c2ddbf389884f9bf21c29ae1f3a26c1a54b
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-04-27 21:01:29 +0530

    Update settingController.dart
    Improved readability

--- App/lib/controllers/settingController.dart
+++ App/lib/controllers/settingController.dart
@@ -6,6 +6,10 @@ import '../routes/route_names.dart';
 
 class SettingController extends GetxController{
   final box = GetStorage();
+
+  /*
+  * @desc Logout of the account clear stored local info + tokens and redirect to main
+  * */
   void logout() async {
     // Remove stored token
     box.erase();
